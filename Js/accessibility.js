7d87bb09ee95fd7f630b3e8a9544f70ace3295f9
KyubikoDayana <bxbdayana24@gmail.com>
2026-05-11 11:36:55 -0500

    Agragar Menu de discapacidad
    Prueba 2 intento 2

--- Js/accessibility.js
+++ Js/accessibility.js
@@ -1,39 +1,28 @@
-// ABRIR / CERRAR PANEL
+// ELEMENTOS
 const container = document.getElementById("accessibility-container");
 const tab = document.getElementById("accessibility-tab");
 
+// TOGGLE PANEL
 tab.addEventListener("click", () => {
   container.classList.toggle("active");
 });
 
 
-// CAMBIO DE TAMAÑO DE TEXTO
+// BOTONES TEXTO
 const buttons = document.querySelectorAll(".acc-btn");
 
 buttons.forEach(btn => {
   btn.addEventListener("click", () => {
-    document.body.classList.remove("large-text", "xlarge-text");
+
+    document.body.classList.remove("acc-large", "acc-xlarge");
 
     if (btn.dataset.size === "large") {
-      document.body.classList.add("large-text");
+      document.body.classList.add("acc-large");
     }
 
     if (btn.dataset.size === "xlarge") {
-      document.body.classList.add("xlarge-text");
+      document.body.classList.add("acc-xlarge");
     }
-  });
-});
 
-
-// CAMBIO DE IDIOMA
-const langButtons = document.querySelectorAll(".acc-lang");
-
-langButtons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const lang = btn.dataset.lang;
-
-    document.querySelectorAll("[data-en]").forEach(el => {
-      el.textContent = el.getAttribute(`data-${lang}`);
-    });
   });
 });
